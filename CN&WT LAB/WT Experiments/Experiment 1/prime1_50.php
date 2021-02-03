6f7cfc36ae00a31b01c2a6bda2b62cc40b34caa6
Alekhya77 <54280241+Alekhya77@users.noreply.github.com>
2021-02-03 10:11:33 +0530

    Create prime1_50.php

---
+++ CN&WT LAB/WT Experiments/Experiment 1/prime1_50.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    for($i = 1; $i <= 50; $i++){
+      $mm = 0;
+      for($j = 2; $j <= $i/2; $j++) {
+                if ($i % $j == 0) {
+                  $mm++;
+                  break;
+                }
+      }
+      if ($mm == 0) {
+                echo"$i ";
+      }
+    }
+    
+  ?>
+</body>
+</html>
